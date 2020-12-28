2623041e49428690cdfd492548308ca816570a47
Justin Rawlings <hrkn@pm.me>
2020-12-28 02:28:42 +0000

    choice drill demo

--- app.py
+++ app.py
@@ -6,21 +6,24 @@ names = [p.name for p in Problem.list]
 
 @app("/app")
 async def serve(q: Q):
-    name = q.client.problem or ""
-    log.error(name)
+    name = q.args.problem or ""
 
-    form = q.page.add(
-        "form",
-        ui.form_card(
+    if not q.client.init:
+        q.client.init = True
+        q.page["form"] = ui.form_card(
             box="1 1 3 10",
             items=[
                 ui.choice_group(
                     "problem",
-                    trigger=True,
                     choices=[ui.choice(name=n, label=n) for n in names],
+                    trigger=True,
+                    value=name,
                 ),
+                ui.label(name),
             ],
-        ),
-    )
+        )
+    else:
+        q.page["form"].items[0].choice_group.value = name
+        q.page["form"].items[1].label.label = name
 
     await q.page.save()
